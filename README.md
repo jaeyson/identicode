a7f67db116d24d164e08ca6348f14a9df8e5e1ed
jaeyson <hello@jaeyson.dev>
2020-02-09 12:57:17 +0700

    update egd dependency and deprecated module functions

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Identicode
+
+### Basic usage for generating avatar profile
+
+```bash
+# install dependencies
+mix deps.get
+
+# go to console
+iex -S mix
+
+# generate png avatar
+iex> Identicode.main("octocat")
+:ok
+```
+
--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Identicode.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :identicode,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :identicode,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,6 +30,8 @@ defmodule Identicode.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:egd, github: "erlang/egd"}
+    ]
   end
 end
